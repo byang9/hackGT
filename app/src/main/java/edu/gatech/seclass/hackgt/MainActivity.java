8bce49427e058fe55da415cdae5f4e18dbdb747d
James Nguyen <jamesnguyen@eduroam-128-61-97-135.eduroam.gatech.edu>
2018-10-20 19:45:01 -0400

    added qr scanner code

--- app/src/main/java/edu/gatech/seclass/hackgt/MainActivity.java
+++ app/src/main/java/edu/gatech/seclass/hackgt/MainActivity.java
@@ -1,9 +1,19 @@
 package edu.gatech.seclass.hackgt;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 public class MainActivity extends AppCompatActivity {
 
