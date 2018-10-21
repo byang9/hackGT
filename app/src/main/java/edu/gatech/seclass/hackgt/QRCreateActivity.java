074f4b43114316da6f88450b8e682e6317eb6a38
ytan49 <ytan49@gatech.edu>
2018-10-20 20:09:14 -0400

    refactored name QRScanActivity->QRCreateActivity

--- app/src/main/java/edu/gatech/seclass/hackgt/QRCreateActivity.java
+++ app/src/main/java/edu/gatech/seclass/hackgt/QRCreateActivity.java
@@ -14,7 +14,7 @@ import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
 
-public class qrScanActivity extends AppCompatActivity {
+public class QRCreateActivity extends AppCompatActivity {
 
     private EditText etInput;
     private Button btnCreateQr;
---
+++ app/src/main/java/edu/gatech/seclass/hackgt/QRScanAcivity.java
@@ -0,0 +1,6 @@
+package edu.gatech.seclass.hackgt;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class QRScanAcivity extends AppCompatActivity {
+}
